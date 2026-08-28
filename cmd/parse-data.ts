8b63db5bc5b92b6734137cc0ff8f050c34eba384
Edward Tanguay (windows) <edward@tanguay.info>
2026-08-28 09:45:03 +0200

    ..videos-concepts-000

--- cmd/parse-data.ts
+++ cmd/parse-data.ts
@@ -1,9 +1,7 @@
-import { parseSeeAlsoLinks } from "./parse-data/parse-see-also-links.ts";
 import { parseArticles } from "./parse-data/parse-articles.ts";
 
 function main(): void {
   console.log("==> Running data parsing pipeline...");
-  parseSeeAlsoLinks();
   parseArticles();
   console.log("==> Data parsing completed successfully.");
 }
